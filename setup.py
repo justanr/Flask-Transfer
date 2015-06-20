29168ba483aeea297a3717e1145df840361e6c53
justanr <alecreiter@gmail.com>
2015-06-20 08:11:49 -0400

    Use tox to run multiple test environments. Test commands are now pytest and tox.

--- setup.py
+++ setup.py
@@ -24,6 +24,29 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
+class ToxTest(TestCommand):
+    user_options = [('tox-args=', 'a', 'Arguments to pass to tox')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        import shlex
+        args = []
+        if self.tox_args:
+            args = shlex.split(self.tox_args)
+
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+
 if __name__ == "__main__":
     setup(
         name='flask-transfer',
@@ -47,6 +70,6 @@ if __name__ == "__main__":
         ],
         install_requires=['Flask'],
         test_suite='test',
-        tests_require=['pytest'],
-        cmdclass={'test': PyTest},
+        tests_require=['tox', 'pytest'],
+        cmdclass={'pytest': PyTest, 'tox': ToxTest},
     )
